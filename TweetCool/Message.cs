8322b81361c7358899cff299bdf43bb8c84b6d6a
Adam <borbelya90@gmail.com>
2020-05-21 12:21:16 +0200

    TweeCool

---
+++ TweetCool/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetCool
+{
+    public class Message
+    {
+        public string Username { get; set; }
+        public string Text { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Message(string username, string text)
+        {
+            Username = username;
+            Text = text;
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
---
+++ TweetCool/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TweetCool.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public static Dictionary<string, List<Message>> Comments = new Dictionary<string, List<Message>>();
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost(
+            [FromForm(Name = "username")]string username,
+            [FromForm(Name = "message")] string message)
+        {
+            List<Message> comments;
+            if (Comments.ContainsKey(username))
+            {
+                comments = Comments[username];
+            }
+            else
+            {
+                comments = new List<Message>();
+                Comments[username] = comments;
+            }
+            comments.Add(new Message(username, message));
+
+
+            Response.Redirect("/Succes");
+
+        }
+    }
+}
